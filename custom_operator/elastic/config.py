8014d68043f010e52a6da6d225066a12429273d3
cucuridas@gamil.com <3310223@naver.com>
2023-05-13 18:02:10 +0900

    smaple 데이터의 위치를 찾기위한 설정

--- custom_operator/elastic/config.py
+++ custom_operator/elastic/config.py
@@ -9,3 +9,4 @@ class ElasticConfig:
     ELASTIC_HOST: str = os.getenv("SERVER_HOST","localhost")
     ELASTIC_PORT: int = os.getenv("SERVER_PORT",9200)
     SET_SSL :  bool = os.getenv("SET_SSL",False)
+    SAMPLE_DATA : str = os.getenv("SAMPLE_DATA")
