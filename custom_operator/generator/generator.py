f01f89256cbca53f046428fcdf37069b0537c84a
cucuridas@gamil.com <3310223@naver.com>
2023-05-30 18:30:58 +0900

    필요요소 random 생성 로직 추가 ->dataframe 작업 필요

---
+++ custom_operator/generator/generator.py
@@ -0,0 +1,184 @@
+from typing import Any
+from airflow.models import BaseOperator
+from airflow.sensors.base import BaseSensorOperator, PokeReturnValue
+from airflow.utils.context import Context
+from elastic.hook import ElasticsearchHook
+from airflow.utils.decorators import apply_defaults
+from airflow.hooks.base import BaseHook
+import redis
+from airflow.models.connection import Connection
+import orjson,socket,random,struct,pandas
+from random import randrange
+from datetime import timedelta
+from datetime import datetime
+
+class GeneratorOperator(BaseOperator):
+    
+    pass
+
+
+class RedisHook(BaseHook):
+    def __init__(self,db:int=None):
+        try:
+            self.conn_info =  BaseHook.get_connection(conn_id="redis")
+
+        except: 
+            self.conn_info = Connection(conn_id="redis",host="operator_custom_tg-redis-1",port=6379)
+        
+        self._db = db
+
+    def get_conn(self):
+        '''
+        redis와 연결하기위한 연결 객체를 만듭니다
+        '''
+        self.conn = redis.Redis(host=self.conn_info.host, port=self.conn_info.port, db=self._db or 0,decode_responses=True)
+        return self.conn
+    
+
+class RedisSensorOperator(BaseSensorOperator):
+    '''
+    Redis에 참조할 sample log가 적재되었는지 확인하기 위한 sensor operator입니다
+    '''
+    template_fields = {"_index_name"}
+
+    @apply_defaults
+    def __init__(self,index_name:str,**kwargs):
+        '''
+        
+        '''
+        #super().__init__(**kwargs)
+        self._index_name = index_name
+        self.conn = RedisHook(db=1).get_conn()
+    
+
+    def poke(self, context: Context) :
+        '''
+        sample data를 확인하기위한 poke 함수를 정의합니다
+        sample data가 미존재시 True를 반환합니다
+        '''
+        sample_data = self.conn.get("sample_{}".format(self._index_name))
+        if sample_data is None:
+            return True
+        else:
+            return False
+
+
+
+
+
+class AddSampleLogOperator(BaseOperator):
+
+    def __init__(self,index_name:str,size:int,conn_id,**kwargs):
+        #super().__init__(**kwargs)
+        self._index_name = index_name
+        self._size = size
+        self.es_conn = ElasticsearchHook(conn_id= conn_id or "local").get_conn()
+        self.redis_conn = RedisHook(db=1).get_conn()
+        
+    def _get_sample_data_from_es(self):
+        sample = self.es_conn.search(index=self._index_name,body={},size=self._size)
+        
+        return sample["hits"]["hits"]
+
+    def _preprocess_sample_data(self,data):
+        key_list = data[0].keys()
+        data = {"key_list":key_list}
+
+
+        
+        return 
+
+    def _insert_sample_data_to_reids(self,data):
+        self.redis_conn.set("sampe_{}".format(self._index_name),data)
+        
+
+    def execute(self, context: Context) -> Any:
+        sample_data = self._get_sample_data_from_es()
+        
+        self._insert_sample_data_to_reids(orjson.dumps(sample_data))
+
+class ConvertDataFrame():
+    def __init__(self,data):
+        source_list = list(map(lambda x:x["_source"],data))
+        self._convert_data = pandas.DataFrame.from_records(source_list)
+
+
+    def extract_case(self):
+        fileds = self._convert_data.columns
+        case_data = {}
+        #print(fileds.to_list(self._convert_data.columns))
+        print(self._convert_data)
+        for filed in fileds :
+            if filed in ("_index","_id","s_IP","d_IP","@timestamp") or filed.startswith("UNKNOWN"):
+                continue
+            else:
+                case_data.setdefault(filed,self._convert_data.groupby())
+        
+        return case_data
+
+class MakeGLog():
+    def __init__(self,data,start_date,end_date):
+        self_data=data
+        self._start_date = start_date
+        self._end_date = end_date
+        
+
+    def _make_radom_IP(self):
+        if self._data.has_key("s_IP") and self._data.has_key("d_IP"):
+            self._random_ip = MakeRandomIP.twice()
+        else :
+            self._random_ip = MakeRandomIP.single()
+
+
+    def _make_random_date(self):
+        self.random_date = MakeRandomDate.random_date(self._start_date,self._end_date)
+
+    def _changeData():
+        pass
+
+
+class MakeRandomData():
+
+    def __init__(self,data,start_date,end_date):
+        self_data=data
+        self._start_date = start_date
+        self._end_date = end_date
+        
+
+    def _make_radom_IP(self):
+        if self._data.has_key("s_IP") and self._data.has_key("d_IP"):
+            self._random_ip = MakeRandomIP.twice()
+        else :
+            self._random_ip = MakeRandomIP.single()
+
+
+    def _make_random_date(self):
+        self.random_date = MakeRandomDate.random_date(self._start_date,self._end_date)
+
+    def _changeData():
+        pass
+
+class MakeRandomDate:
+    def random_date(start:datetime, end:datetime):
+        """
+        This function will return a random datetime between two datetime 
+        objects.
+        """
+        delta = end - start
+        int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+        random_second = randrange(int_delta)
+        return start + timedelta(seconds=random_second)
+
+class MakeRandomIP:
+    def single()->str:
+        random_ip = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
+        return random_ip
+
+    def twice()->list:
+
+        random_ip = []
+        for i in range(2):
+            random_ip.append(socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff))))
+        return random_ip
+    
+
--- custom_operator/test/generator/test_generator.py
+++ custom_operator/test/generator/test_generator.py
@@ -2,23 +2,45 @@ import sys
 sys.path.append("/Users/cucuridas/Desktop/operator_custom_tg")
 sys.path.append("/opt/airflow/dags")
 
-from generator.sample import *
+from generator.generator import *
+from datetime import datetime
 
 
-def test_sample_logs():
-    assert SampleLogs("docker_elastic").check("test")
 
 
-def test_insert_sample_logs():
-    SampleLogs("docker_elastic","ips").insert()
+def test_RedisSensorOperaotr():
+    test_obj = RedisSensorOperator(index_name="firewall")
+    #test_obj.conn.set("sample_firewall","test")
+    assert test_obj.poke(context=None)
+    
 
-def test_make_index_obj():
-    MakeIndex("docker_elastic","firewall")()
+def test_AddSampleLogOperator():
+    test_obj = AddSampleLogOperator(index_name="ips",size=10000,conn_id="docker_elastic")
+    test_obj.execute(context=None)
+
+
+def test_MakeRandomIP():
+    test_obj = MakeRandomIP.twice()
+    print(test_obj)
+
+def test_MakeRadomDate():
+    start = datetime(2023,5,30,15,14)
+    end = datetime(2023,5,30,15,15)
+    test_obj = MakeRandomDate.random_date(start,end)
+
+    print(test_obj)
+
+def test_ConvertDataFrame():
+    
+    mockup_obj = AddSampleLogOperator(index_name="ips",size=10000,conn_id="docker_elastic")
+    data = mockup_obj._get_sample_data_from_es()
+
+    test_obj = ConvertDataFrame(data)
+    print(test_obj.extract_case())
 
 if __name__ == "__main__":
-    #test_sample_logs()
-    #test_insert_sample_logs()
-    # test_make_ilm_polity()
-    # test_index_template()
-    #test_make_index_obj()
-    print( SampleLogs("docker_elastic","waf").check())
+    test_ConvertDataFrame()
+    #test_MakeRadomDate()
+    #test_MakeRandomIP()
+    #test_AddSampleLogOperator()
+    #test_RedisSensorOperaotr()
