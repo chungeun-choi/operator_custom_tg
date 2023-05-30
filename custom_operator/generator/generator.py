93de7692ca634d5ce8b0b46ff5e00b2df46b414d
cucuridas <3310223@naver.com>
2023-05-31 00:54:35 +0900

    GenerateLogDatas 개발 완료 -> 테스트 필요

--- custom_operator/generator/generator.py
+++ custom_operator/generator/generator.py
@@ -1,3 +1,4 @@
+import os
 from typing import Any
 from airflow.models import BaseOperator
 from airflow.sensors.base import BaseSensorOperator, PokeReturnValue
@@ -9,12 +10,18 @@ import redis
 from airflow.models.connection import Connection
 import orjson,socket,random,struct,pandas
 from random import randrange
-from datetime import timedelta
-from datetime import datetime
+from datetime import timedelta,datetime
+import numpy as np
 
 class GeneratorOperator(BaseOperator):
-    
-    pass
+    def __init__(self,index_name:str,size:int,start_date:datetime,end_date:datetime,conn_id:str,**kwargs):
+        super().__init__(**kwargs)
+        self._created_data = GenerateLogDatas(size=size,index_name=index_name,start_date=start_date,end_date=end_date)
+        self.es_conn = ElasticsearchHook(conn_id= conn_id or "local").get_conn()
+        
+    def execute(self):
+        #self.es_conn.bulk
+        pass
 
 
 class RedisHook(BaseHook):
@@ -67,8 +74,18 @@ class RedisSensorOperator(BaseSensorOperator):
 
 
 class AddSampleLogOperator(BaseOperator):
+    '''
+    redis에 적재되어진 sample 데이터가 없을 경우 elasticsearch에서 데이터를 가져와
+    적재합니다
+    '''
 
     def __init__(self,index_name:str,size:int,conn_id,**kwargs):
+        '''
+        params:
+            index_name(str): elasticsearch index 이름,
+            size(int): sample 데이터의 양
+            conn_id(str): elasticsearch conntion 정보를 가져올 id
+        '''
         #super().__init__(**kwargs)
         self._index_name = index_name
         self._size = size
@@ -93,7 +110,10 @@ class AddSampleLogOperator(BaseOperator):
         self.redis_conn.set("sampe_{}".format(self._index_name),data)
         
 
-    def execute(self, context: Context) -> Any:
+    def execute(self, context: Context) :
+        '''
+        operator 객체를 생성하고 task를 execute하는 함수
+        '''
         get_data = self._get_sample_data_from_es()
         sample_case_data = self._preprocess_sample_data(get_data)
         self._insert_sample_data_to_reids(orjson.dumps(sample_case_data))
@@ -117,37 +137,28 @@ class ConvertDataFrame():
         
         return case_data
 
-class MakeGLog():
-    def __init__(self,data,start_date,end_date):
-        self_data=data
-        self._start_date = start_date
-        self._end_date = end_date
+
+      
         
-
-    def _make_radom_IP(self):
-        if self._data.has_key("s_IP") and self._data.has_key("d_IP"):
-            self._random_ip = MakeRandomIP.twice()
-        else :
-            self._random_ip = MakeRandomIP.single()
-
-
-    def _make_random_date(self):
-        self.random_date = MakeRandomDate.random_date(self._start_date,self._end_date)
-
-    def _changeData():
-        pass
-
-
 class MakeRandomData():
 
-    def __init__(self,data,start_date,end_date):
-        self_data=data
+    def __init__(self,index_name:str,start_date:datetime,end_date:datetime):
+        self._index_name = index_name
+        self._redis_sample = RedisHook(1).get_conn().get("sampe_{}".format(self._index_name))
         self._start_date = start_date
-        self._end_date = end_date
-        
+        self._end_date = end_date       
+
+    def make_data(self)->dict:
+        '''
+        해당 클래스내의 내장 함수를 통해 랜덤 데이터를 생성합니다
+        랜덤 데이터 생성 시 reids에 적재되어진 sample 내용을 참조하여 생성하게됩니다
+        '''
+        self._make_radom_IP()
+        self._make_random_date()
+        return self._changeData()
 
     def _make_radom_IP(self):
-        if self._data.has_key("s_IP") and self._data.has_key("d_IP"):
+        if "s_IP" in self._redis_sample["key_list"] and "d_IP" in self._redis_sample["key_list"]:
             self._random_ip = MakeRandomIP.twice()
         else :
             self._random_ip = MakeRandomIP.single()
@@ -156,14 +167,35 @@ class MakeRandomData():
     def _make_random_date(self):
         self.random_date = MakeRandomDate.random_date(self._start_date,self._end_date)
 
-    def _changeData():
-        pass
-
+    def _changeData(self):
+        data = {}
+        for field in self._redis_sample["key_list"]:
+            if field.startswith("UNKNOWN"):
+                data.setdefault(field,bytearray(os.urandom(1000000)))
+            elif field in ("_index","_id","s_IP","d_IP","@timestamp"):
+                continue
+            else:
+                random_value = np.random.choice(self._redis_sample["case_data"][field], size=1)
+                data.setdefault(field,random_value)
+        return data
+        
+class GenerateLogDatas(MakeRandomData):
+    def __init__(self,size,**kwargs):
+        self._size = size
+        super().__init__(**kwargs)
+        
+    def generate(self):
+        
+        generate_log = [self._changeData() for count in range(self._size)]
+        
+        return generate_log
+        
+        
+        
 class MakeRandomDate:
     def random_date(start:datetime, end:datetime):
         """
-        This function will return a random datetime between two datetime 
-        objects.
+        시작날짜와 종료날짜를 입력받아 랜덤한 날짜를 전달하는 함수입니다
         """
         delta = end - start
         int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
